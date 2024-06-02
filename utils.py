c8110bb15e6aebce28ca32034cfd523d7c3f3a6b
= <=>
2024-06-02 23:31:26 +0300

    Initial commit

--- utils.py
+++ utils.py
@@ -71,53 +71,6 @@ def image_to_desc(base64_image, openai_key, pl_theme="None"):
     print(response.json())
     return res1
 
-    
-# def image_to_desc1(base64_image, openai_key, pl_theme="None"):
-#     openai.api_key = openai_key
-
-#     headers = {
-#         "Content-Type": "application/json",
-#         "Authorization": f"Bearer {openai.api_key}"
-#     }
-
-#     payload = {
-#         "model": "gpt-4-vision-preview",
-#         "messages": [
-#             {
-#                 "role": "user",
-#                 "content":  f"""
-#     Create a list of 20 songs for this playlist theme: {pl_theme}.
-#     The form of your response should be 'Description$&$List_of_songs', where List_of_songs is in this format ("song name": artist&).
-#     """
-#                 # "content": f"""
-#                 # can you create a list of 20 songs that would match the setting portrayed in this image and/or to set the vibe 
-#                 # portrayed by this image use it to select recommendations.
-#                 # Also make sure to favour songs that are higher rated and more popular. I need a minimum of 15 songs.
-#                 # I just want you to list the results in this format ("song name": artist&), separated by commas and nothing 
-#                 # else in your response. Additionally give me a little description of the image as a prompt to pass on to 
-#                 # DALL-E to make a cartoony version of the prompt as a playlist cover (put this part at the beginning of your 
-#                 # response and separate from the list using $&$). 
-#                 # Just a reminder: the form of your response should be 'Description$&$List_of_songs', where List_of_songs is in this format ("song name": artist&)
-#                 # """
-#             },
-#             {
-#                 "role": "user",
-#                 "content": f"data:image/jpeg;base64,{base64_image}"
-#             }
-#         ],
-#         "max_tokens": 500
-#     }
-
-#     response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
-#     print(response.json())  # For debugging
-#     try:
-#         res1 = response.json()['choices'][0]['message']['content']
-#         return res1
-#     except KeyError:
-#         print("KeyError: 'choices' not found in the response.")
-#         return None
-
-
 def create_playlist_fun(sp, username, playlist_name, playlist_description):
     playlists = sp.user_playlist_create(username, playlist_name, description = playlist_description, public=True)
 
